fa2cb94133c2079d53885768bcfe3a2face0a33d
Manit <manit.manu4@gmail.com>
2022-10-12 19:01:21 +0530

    jdbc_second_12-10

--- src/jdbc/JDBCReview.java
+++ src/jdbc/JDBCReview.java
@@ -1,4 +1,124 @@
 package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.protocol.Resultset;
+
+public class JDBCReview {//This is the example for deleting particular row or rows at once since "where" clause is used in the sql query...
+
+	public static void main(String[] args) throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cohart20_review", "root", "root");
+		
+		PreparedStatement pstmt = conn.prepareStatement("select * from candidate");
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			
+			System.out.println(rs.getInt(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getString(3));	
+			
+		}
+		
+		
+	}
+}
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//
+//import com.mysql.cj.protocol.Resultset;
+//
+//public class JDBCReview {//This is the example for deleting particular row or rows at once since "where" clause is used in the sql query...
+//
+//	public static void main(String[] args) throws Exception {
+//		
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cohart20_review", "root", "root");
+//		
+//		PreparedStatement pstmt = conn.prepareStatement("select * from candidate");
+//		
+//		ResultSet rs = pstmt.executeQuery();
+//		
+//		rs.next();
+//		
+//		System.out.println(rs.getInt(1));
+//		System.out.println(rs.getString(2));
+//		System.out.println(rs.getString(3));
+//		
+//		rs.next();
+//		
+//		System.out.println(rs.getInt(1));
+//		System.out.println(rs.getString(2));
+//		System.out.println(rs.getString(3));
+//		
+//		rs.next();
+//		
+//		System.out.println(rs.getInt(1));
+//		System.out.println(rs.getString(2));
+//		System.out.println(rs.getString(3));
+//	}
+//}
+
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.Statement;
+//import java.util.Scanner;
+//
+//public class JDBCReview {//This is the example for deleting particular row or rows at once since "where" clause is used in the sql query...
+//
+//	public static void main(String[] args) throws Exception {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter the name to delete record/s");
+//		
+//		String enteredName = sc.next();
+//		
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cohart20_review", "root", "root");
+//		
+//		PreparedStatement pstmt = conn.prepareStatement("delete from candidate where name = ?");
+//		
+//		pstmt.setString(1, enteredName);
+//		
+//		pstmt.execute();
+//		
+//	}
+//}
+
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//
+//public class JDBCReview {//This is the example for deleting all the rows since I did not specify any particular row with "where" clause... 
+//
+//	public static void main(String[] args) throws Exception {
+//		
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cohart20_review", "root", "root");
+//		
+//		PreparedStatement pstmt = conn.prepareStatement("delete from candidate");
+//		
+//		pstmt.execute();	
+//	}
+//}
+
 //import java.sql.Connection;
 //import java.sql.DriverManager;
 //import java.sql.PreparedStatement;
@@ -37,7 +157,7 @@ package jdbc;
 
 
 
-//
+
 //import java.sql.Connection;
 //import java.sql.DriverManager;
 //import java.sql.PreparedStatement;
@@ -146,26 +266,26 @@ package jdbc;
 
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
-public class JDBCReview {//This program is creating table from the Java code...
-
-	public static void main(String[] args) throws Exception {
-		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cohart20_review", "root", "root");
-		
-		Statement stmt = conn.createStatement();
-		
-		stmt.executeUpdate("create table candidate(c_id int primary key, name varchar(30), email varchar(30))");
-//		executeUpdate() is used to mention the sql query and this method is used to either create table, update table, delete table
-//		or insert into table...
-	}
-
-}
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.Statement;
+//
+//public class JDBCReview {//This program is creating table from the Java code...
+//
+//	public static void main(String[] args) throws Exception {
+//		
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cohart20_review", "root", "root");
+//		
+//		Statement stmt = conn.createStatement();
+//		
+//		stmt.executeUpdate("create table candidate(c_id int primary key, name varchar(30), email varchar(30))");
+////		executeUpdate() is used to mention the sql query and this method is used to either create table, update table, delete table
+////		or insert into table...
+//	}
+//
+//}
 
 
 
